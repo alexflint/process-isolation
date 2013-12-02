b028abf15c61021560378801fc9543a104ffb3ab
alexflint <alex.flint@gmail.com>
2013-12-01 22:38:48 -0500

    Version 0.13

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(name='process_isolation',
       description='Elegant per-module process isolation in pure python',
-      version='0.12',
+      version='0.13',
       author='Alex Flint',
       author_email='alex.flint@gmail.com',
       url='http://alexflint.github.io/process-isolation/',
